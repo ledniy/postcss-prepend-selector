77e26b96ab64d4a0bf21ca2ab4e4af46ea0073ad
exsetnamor <rt@filefabrik.com>
2021-05-19 10:21:15 +0200

    # 0.4.0 (2021-05-19)
    Added: exclude .classnames and #id's
    Added: excludePart for .classnames and #id's start with

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-prepend-selector', function (opts) {
+module.exports = postcss.plugin('prepend-selector-postcss', function (opts) {
+
     opts = opts || {};
+    opts.exclude = !opts.exclude ? null : opts.exclude;
+    opts.excludePart = !opts.excludePart ? null : opts.excludePart;
     return function (css) {
         css.walkRules(function (rule) {
-            rule.selectors = rule.selectors.map( function (selector) {
+            rule.selectors = rule.selectors.map(function (selector) {
                 if(/^([0-9]*[.])?[0-9]+\%$|^from$|^to$/.test(selector)) {
                     // This is part of a keyframe
                     return selector;
                 }
 
-                if (selector.startsWith(opts.selector.trim())) {
+                if(selector.startsWith(opts.selector.trim())) {
                     return selector;
+                }
+                /** FileFabrik change have ignores
+                 * exclude: ['#exc '],
+                 **/
+                if(opts.exclude && Array.isArray(opts.exclude)) {
+                    /** append the space after because of full term **/
+                    const s = selector.split(' ')[0] + ' ';
+                    if(opts.exclude.indexOf(s) !== -1) {
+                        return selector;
+                    }
+                }
+                /** FileFabrik change have ignores    excludePart: ['.grid'] **/
+                if(opts.excludePart && Array.isArray(opts.excludePart)) {
+                    let found = false;
+                    opts.excludePart.forEach(function (excludePart) {
+                        if(selector.startsWith(excludePart))
+                            found = true;
+                        return found;
+                    });
+                    if(found)
+                        return selector;
                 }
 
                 return opts.selector + selector;
